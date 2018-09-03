b701527444b967a05d2ea37c33428fae32ed31d2
MansiSharma1 <mansisharma1006@gmail.com>
2018-09-03 18:19:07 +0530

    Python Code

---
+++ EchoBot.py
@@ -0,0 +1,43 @@
+"""
+This bot listens to port 5002 for incoming connections from Facebook. It takes
+in any messages that the bot receives and echos it back.
+"""
+from flask import Flask, request
+from pymessenger.bot import Bot
+
+app = Flask(__name__)
+
+ACCESS_TOKEN = "EAAEE0K1fxZAMBAEGYJaZArDxWVqxeLUTEN7WJGJ9s7SgnyYogtnl6w1n87leUxIS1wrJioukvZCYafZAnIJyM0yy0oZAviFs0ZBP7BHOJ16IszAx0XZCUpNFZBLQZCOUScTnZAjqiJ2ULsHtp4ZAOZCgpHDeqgaUCuC6fCGkOxIIQGXoZAkQWqRqWEyF9JtauwkhnZBW4ZD"
+VERIFY_TOKEN = "h"
+bot = Bot(ACCESS_TOKEN)
+
+
+@app.route("/", methods=['GET', 'POST'])
+def hello():
+    if request.method == 'GET':
+        if request.args.get("hub.verify_token") == VERIFY_TOKEN:
+            return request.args.get("hub.challenge")
+        else:
+            return 'Invalid verification token'
+
+    if request.method == 'POST':
+        output = request.get_json()
+        for event in output['entry']:
+            messaging = event['messaging']
+            for x in messaging:
+                if x.get('message'):
+                    recipient_id = x['sender']['id']
+                    if x['message'].get('text'):
+                        message = x['message']['text']
+                        bot.send_text_message(recipient_id, message)
+                    if x['message'].get('attachments'):
+                        for att in x['message'].get('attachments'):
+                            bot.send_attachment_url(recipient_id, att['type'], att['payload']['url'])
+                else:
+                    pass
+        return "Success"
+
+
+if __name__ == "__main__":
+    app.run(port=5002, debug=True)
+
